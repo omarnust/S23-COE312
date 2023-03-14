c3d312c9e3aa544d38287605d0a9464f9b000e0f
Omar Arif <oarif@CEN-F-oarif.local>
2023-03-14 20:36:16 +0400

    socket

---
+++ Sockets/SensorClient/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main (String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		TCPClient client = new TCPClient("10.25.141.24", 62033);
+		client.t.join();
+	}
+
+}
