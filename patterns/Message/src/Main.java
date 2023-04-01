88d0c1fe66a7238fc9fc399d610dfb070699abf6
Omar Arif <oarif@CEN-F-oarif.local>
2023-04-01 16:44:43 +0400

    event based architecture

---
+++ patterns/Message/src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		TCP_Client phone = new TCP_Client("192.168.68.110", 61525); // to start the server
+		Clock c1 = new Clock(1);
+		Clock c2 = new Clock(5);
+		Clock c3 = new Clock(10);		
+		ConcreteSubject[] subjects = {phone,c1, c2};// c3};
+		
+		// Uncomment one of the following observer
+		SimpleObserver o1 = new SimpleObserver(subjects);	
+		ComplexObserver o2 = new ComplexObserver(subjects);		
+	    MoreComplexObserver o3 = new MoreComplexObserver(subjects);
+		
+		
+		
+		//UI ui = new UI();
+		//ConcreteSubject[] subjects = {ui};
+		
+		//UIClient c1 = new UIClient(subjects);
+		//UIClient c2 = new UIClient(subjects);
+		//ui.UIWrite("Hello from the UI Subject");
+
+	}
+
+}
